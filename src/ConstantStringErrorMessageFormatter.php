793750dc1c1e561ded68b8c984e66bd5e6d1d35d
Dave Liddament <dave@lampbristol.com>
2023-12-14 00:05:14 +0000

    DEPRECATE ConstantStringErrorMessageFormatter as this is no longer needed

--- src/ConstantStringErrorMessageFormatter.php
+++ src/ConstantStringErrorMessageFormatter.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace DaveLiddament\PhpstanRuleTestHelper;
 
+/**
+ * @deprecated From v 0.3.0 this is no longer needed.`
+ *
+ * Before 0.3.0 `getErrorFormatter` had to return an instance of `ErrorMessageFormatter`.
+ *
+ * public function getErrorFormatter() {
+ *    return new ConstantStringErrorMessageFormatter('My error message');
+ * }
+ *
+ * Since 0.3.0 you can return a string or an instance of `ErrorMessageFormatter`.
+ *
+ * public function getErrorFormatter() {
+ *   return 'My error message';
+ * }
+ */
 class ConstantStringErrorMessageFormatter extends ErrorMessageFormatter
 {
     public function __construct(
