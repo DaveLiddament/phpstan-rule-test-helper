1ea2f22577b8afca0df2d8e4bf46f910e698aa1c
Simon Podlipsky <simon@podlipsky.net>
2024-12-02 18:12:33 +0100

    Require phpstan v2

--- tests/Integration/NoGotoPhpstanRule.php
+++ tests/Integration/NoGotoPhpstanRule.php
@@ -8,6 +8,7 @@ use PhpParser\Node;
 use PhpParser\Node\Stmt\Goto_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
 
 /** @implements Rule<Goto_> */
 final class NoGotoPhpstanRule implements Rule
@@ -19,6 +20,10 @@ final class NoGotoPhpstanRule implements Rule
 
     public function processNode(Node $node, Scope $scope): array
     {
-        return ['goto statement is not allowed. Label: '.$node->name->toString()];
+        return [
+            RuleErrorBuilder::message('goto statement is not allowed. Label: '.$node->name->toString())
+                ->identifier('test.gotoNotAllowed')
+                ->build(),
+        ];
     }
 }
